fc5239acb299296ba1871d5fed7e052cf6331a6d
sogue <61759231+sogue@users.noreply.github.com>
2023-04-23 22:44:05 +0200

    Add ChatGpt connection

--- Bots/EchoBot.cs
+++ Bots/EchoBot.cs
@@ -3,32 +3,53 @@
 //
 // Generated with EchoBot .NET Template version v4.17.1
 
-using System.Collections.Generic;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace EchoBot.Bots
 {
     public class EchoBot : ActivityHandler
     {
-        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        private readonly IConfiguration _configuration;
+
+        public EchoBot(IConfiguration c)
         {
-            var replyText = $"Echo: {turnContext.Activity.Text}";
-            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            _configuration = c;
         }
 
-        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText = "Hello and welcome!";
-            foreach (var member in membersAdded)
-            {
-                if (member.Id != turnContext.Activity.Recipient.Id)
-                {
-                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
-                }
-            }
+            var openAiKey = _configuration["OpenAi"] ?? Environment.GetEnvironmentVariable("OpenAi");
+            var client    = new RestClient("https://api.openai.com/v1/completions");
+            var request   = new RestRequest();
+            request.AddHeader("Content-Type",  "application/json");
+            request.AddHeader("Authorization", "Bearer " + openAiKey);
+
+            var requestBody = new
+                              {
+                                  model             = "text-davinci-003",
+                                  prompt            = turnContext.Activity.Text,
+                                  temperature       = 0.9,
+                                  max_tokens        = 4000,
+                                  top_p             = 1,
+                                  frequency_penalty = 0,
+                                  presence_penalty  = 0.6,
+                                  stop              = new string[] { " Human:", " AI:" }
+                              };
+            request.AddParameter("application/json", JsonConvert.SerializeObject(requestBody),
+                                 ParameterType.RequestBody);
+
+            var response        = await client.ExecutePostAsync(request, cancellationToken: cancellationToken);
+            var responseContent = JsonConvert.DeserializeObject<dynamic>(response.Content);
+            var generatedText   = responseContent.choices[0].text.ToString().Trim();
+
+            await turnContext.SendActivityAsync(MessageFactory.Text(generatedText), cancellationToken);
         }
     }
 }
